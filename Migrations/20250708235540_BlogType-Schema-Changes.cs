aa78681b72ae68a9272a69b5eddd3b4d4cfae659
SHARMAT9 <sharmat9@mymacewan.ca>
2025-07-08 18:18:25 -0600

    Exercise 3

---
+++ Migrations/20250708235540_BlogType-Schema-Changes.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogPostApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogTypeSchemaChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogtypes");
+
+            migrationBuilder.CreateTable(
+                name: "BlogType",
+                columns: table => new
+                {
+                    BlogTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogType", x => x.BlogTypeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogType");
+
+            migrationBuilder.CreateTable(
+                name: "Blogtypes",
+                columns: table => new
+                {
+                    BlogTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogtypes", x => x.BlogTypeId);
+                });
+        }
+    }
+}
--- Models/BlogType.cs
+++ Models/BlogType.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BlogPostApplication.Models
+[Table("BlogType")]  // Set table name to BlogType
+public class BlogType
 {
-  public class BlogType
-    {
-        public int BlogTypeId { get; set; }    // Primary Key
-        public int Status { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
+    [Key]  // Primary key
+    public int BlogTypeId { get; set; }  // BlogTypeId as primary key
+
+    [Required]  // Make Status required
+    public int Status { get; set; }  // Assuming Status is an int
+
+    [Required]  // Make Name required
+    [MaxLength(20)]  // Maximum length of 20 characters
+    public string Name { get; set; }
+
+    [MaxLength(400)]  // Maximum length of 400 characters for Description
+    public string Description { get; set; }
 }
